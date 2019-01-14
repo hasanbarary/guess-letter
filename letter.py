c33353993c12183d1bc0ad191bf937d32744090b
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-14 18:49:34 +0300

    add rule win score game_level
    add rule win score game_level

--- letter.py
+++ letter.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3.6
 from random import randint
 import os
-def score():
-    score=100
-    if level == 10:
-        score-=10
-    elif level == 5:
-        score-=20
+def rule(user_char):
+    if len(user_char)>1:
+        print("You just Enter one character Please Enter To Continue")
+        input()
+        return True
+    elif  str(user_char).isalnum:
+        print("You just Enter character Not Number Please Enter To Continue")
+        input()
+        return True
     else:
-        score-=35
-    return score()
+        return False
+def win(user_char,rand_char,final_score):
+    if user_char==rand_char:
+        final_score+=20
+        os.system('clear')
+        print(''.join(user_words))
+        print("congratulations,You are Win :) :)")
+        print("your Final Score is {}".format(final_score))
+        return True
+    else:
+        return False
+def score():
+    if health == 10:
+        score=3
+    elif health == 5:
+        score=5
+    else:
+         score=10
+    return score
 def game_level():
     answer=int(input("Enter Your Level For Game --> \n **** 1.EASY **** \n **** 2.Medium **** \n **** 3.HARD ****\n ---> "))
     if answer == 1:
@@ -27,15 +47,9 @@ guess_count=0
 health=game_level()
 user_words=[]
 wrong_char=[]
+score=int(score())
 final_score=0
 mines_score=0
-if health == 10:
-    score=3
-elif health == 5:
-    score=5
-else:
-    score=10
-
 for char in rand_words:
     user_words+='_'
 while True:
@@ -47,9 +61,7 @@ while True:
     print('\nEnter below list pre (not use again) : \n')
     print(" , ".join(wrong_char))
     user_char=input("\nEnter Your guess --> ")
-    if len(user_char)>1 or not "".isalpha:
-        print("You just Enter one character Please Enter To Continue")
-        input()
+    if rule(user_char):
         continue
     for rand_number in range(0,len(rand_words)):
         if user_char == rand_words[rand_number]:
@@ -64,11 +76,6 @@ while True:
     if not health:
         print("Game Over :( Im Sorry :( Try Again)")
         break
-    elif ''.join(user_words) == ''.join(rand_words):
-        final_score+=20
-        os.system('clear')
-        print(''.join(user_words))
-        print("congratulations,You are Win :) :)")
-        print("your Final Score is {}".format(final_score))
-        print()
+    elif win(''.join(user_words),''.join(rand_words),final_score):
         break
+    print()
